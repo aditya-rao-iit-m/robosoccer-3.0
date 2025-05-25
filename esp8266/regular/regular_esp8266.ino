190089d6428bf90fb4a6065fa3a02e23a4d384c5
Aditya Rao - IIT Madras <166688891+aditya-rao-iit-m@users.noreply.github.com>
2025-05-25 21:03:12 +0530

    Update regular_esp8266.ino

--- esp8266/regular/regular_esp8266.ino
+++ esp8266/regular/regular_esp8266.ino
@@ -1,6 +1,8 @@
-// ROBOSOCCER 3.0 - Paradox 2025
-// ESP8266 Code Developed By Aditya Rao
-// IIT Madras
+// Base Code for RoboSoccer 3.0
+// ESP8266 Regular Wheels
+// 23f3000019@es.study.iitm.ac.in - Aditya Rao
+// Chief Mentor - Dr.Janakiraman Viraraghavan
+// Electrical Engineering Department - IIT Madras
 
 
 #include <SocketIOclient.h>
