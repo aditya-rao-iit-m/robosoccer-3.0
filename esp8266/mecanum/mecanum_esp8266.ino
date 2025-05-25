529edbd0e9be6cfd3766304b78d24a3384d3a87f
Aditya Rao - IIT Madras <166688891+aditya-rao-iit-m@users.noreply.github.com>
2025-05-25 19:26:06 +0530

    Add files via upload

---
+++ esp8266/mecanum/mecanum_esp8266.ino
@@ -0,0 +1,156 @@
+// ROBOSOCCER 3.0 - Paradox 2025
+// ESP8266 Code Developed By Aditya Rao
+// Indian Institute of Technology Madras
+
+
+#include <SocketIOclient.h>
+#include <WebSockets.h>
+#include <WebSockets4WebServer.h>
+#include <WebSocketsClient.h>
+#include <WebSocketsVersion.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include "index.h"
+
+#define CMD_STOP 0
+#define CMD_FORWARD 1
+#define CMD_BACKWARD 2
+#define CMD_LEFT 4
+#define CMD_RIGHT 8
+
+
+#define IN1_PIN 4   // D2
+#define IN2_PIN 14  // D5
+#define IN3_PIN 12  // D6
+#define IN4_PIN 13  // D7
+
+const char* ssid = "robosoccer";     // Change this to something of your own
+const char* password = "iitmadras";  // Change this to something of your own
+
+ESP8266WebServer server(80);                        // Web server on port 80
+WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Soccer Robot Disconnected\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      //Serial.printf("[%u] Received text: %s\n", num, payload);
+      String angle = String((char*)payload);
+      int command = angle.toInt();
+      Serial.print("command: ");
+      Serial.println(command);
+
+      switch (command) {
+        case CMD_STOP:
+          Serial.println("Stop");
+          stop();
+          break;
+        case CMD_FORWARD:
+          Serial.println("Move Forward");
+          moveForward();
+          break;
+        case CMD_BACKWARD:
+          Serial.println("Move Backward");
+          moveBackward();
+          break;
+        case CMD_LEFT:
+          Serial.println("Turn Left");
+          turnLeft();
+          break;
+        case CMD_RIGHT:
+          Serial.println("Turn Right");
+          turnRight();
+          break;
+        default:
+          Serial.println("NA");
+      }
+
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi Hotspot...");
+  }
+  Serial.println("Connected to WiFi Hotspot");
+
+  // Initialize WebSocket server
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  // Serve a basic HTML page with JavaScript to create the WebSocket connection
+  server.on("/", HTTP_GET, []() {
+    Serial.println("Web Server: Received a web page request");
+    String html = HTML_CONTENT; 
+    server.send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.print("ESP8266 Soccer Robot Web Server's IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Handle client requests
+  server.handleClient();
+
+  // Handle WebSocket events
+  webSocket.loop();
+
+}
+
+void moveForward() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, HIGH);
+}
+
+void moveBackward() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void turnLeft() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void stop() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, LOW);
+}
