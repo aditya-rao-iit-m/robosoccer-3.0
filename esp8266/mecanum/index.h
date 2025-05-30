c1f39498b2aa88145bc1d6fb3845744305d3918e
Aditya Rao - IIT Madras <166688891+aditya-rao-iit-m@users.noreply.github.com>
2025-05-30 07:33:09 +0530

    Update index.h

--- esp8266/mecanum/index.h
+++ esp8266/mecanum/index.h
@@ -9,7 +9,7 @@ const char *HTML_CONTENT = R"=====(
 <!DOCTYPE html>
 <html>
 <head>
-<title>ROBOSOCCER 3.0 - ESP8266 Robot Control</title>
+<title>ROBOSOCCER 3.0 - ESP8266 Mecanum Control</title>
 
 <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=1, user-scalable=no">
 <style type="text/css">
@@ -18,7 +18,9 @@ html, body {
   margin: 0;
   padding: 0;
   text-align: center;
-  font-size: 24px;
+  font-size: 20px;
+  font-family: arial;
+  background: #000000;
 }
 
 button {
@@ -26,16 +28,19 @@ button {
   width: 90%;
   margin: 10px auto;
   padding: 20px;
-  background-color: #E7A832;
+  background-color: #909090;
   border: none;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0,0,0,0.2);
   font-weight: bold;
   display: block;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none;     /* IE 10 and IE 11 */
+  user-select: none;         /* Standard syntax */
 }
 
 button:active {
-  background-color: #BE7D03;
+  background-color: #dedede;
   transform: scale(0.97);
 }
 </style>
@@ -69,7 +74,7 @@ function ws_onmessage(e_msg) {
 }
 
 function ws_onopen() {
-  document.getElementById("ws_state").innerHTML = "CONNECTED";
+  document.getElementById("ws_state").innerHTML = "Connected";
   document.getElementById("wc_conn").innerHTML = "Disconnect";
 }
 
@@ -85,7 +90,7 @@ function ws_onclose() {
 function wc_onclick() {
   if(ws == null) {
     ws = new WebSocket("ws://" + window.location.host + ":81");
-    document.getElementById("ws_state").innerHTML = "CONNECTING";
+    document.getElementById("ws_state").innerHTML = "Connecting...";
     ws.onopen = ws_onopen;
     ws.onclose = ws_onclose;
     ws.onmessage = ws_onmessage;
@@ -104,22 +109,67 @@ window.onload = init;
 </script>
 </head>
 <body>
-<h3 style="margin-top: 5px; margin-bottom: 4px;">ROBOSOCCER 3.0</h3>
-<h4 style="margin-top: 1px; color: #404040;">ESP8266 Wi-Fi Mecanum Robot - Aditya Rao</h4>
-<div style="width: 100%; max-width: 420px; margin: auto;">
-  <button data-cmd="1">FORWARD</button>
-  <button data-cmd="2">BACKWARD</button>
-  <button data-cmd="4">STRAFE LEFT</button>
-  <button data-cmd="8">STRAFE RIGHT</button>
-  <button data-cmd="16">FORWARD LEFT</button>
-  <button data-cmd="32">FORWARD RIGHT</button>
-  <button data-cmd="64">BACKWARD LEFT</button>
-  <button data-cmd="128">BACKWARD RIGHT</button>
-  <button data-cmd="256">ROTATE LEFT</button>
-  <button data-cmd="512">ROTATE RIGHT</button>
+<h3 style="margin-top: 5px; margin-bottom: 4px; color: #ffff00;">ROBOSOCCER 3.0</h3>
+<h4 style="margin-top: 1px; margin-bottom: 2px; color: #606060;">ESP8266 Wi-Fi Mecanum Robot</h4>
+<h5 style="margin-top: 1px; margin-bottom: 2px; color: #909090;">Aditya Rao</h4>
+
+<div style="width: 100%; max-width: 800px; margin: auto;">
+<center>
+<table>
+<tr>
+  <td>
+  <button data-cmd="16" style="background-color:#4d4dff;">FORWARD LEFT</button>
+  </td>
+  <td>
+  <button data-cmd="1" style="background-color:#00cc00;">FORWARD</button>
+  </td>
+  <td>
+  <button data-cmd="32" style="background-color:#4d4dff;">FORWARD RIGHT</button>
+  </td>
+</tr>
+<tr>
+  <td>
+  <button data-cmd="4" style="background-color:#e68a00;">STRAFE LEFT</button>
+  </td>
+  <td>
+  <button data-cmd="0" style="background-color:#ff0000;">STOP</button>
+  </td>
+  <td>
+  <button data-cmd="8" style="background-color:#aaaa55;">STRAFE RIGHT</button>
+  </td>
+</tr>
+<tr>
+  <td>
+  <button data-cmd="64" style="background-color:#4d4dff;">BACKWARD LEFT</button>
+  </td>
+  <td>
+  <button data-cmd="2" style="background-color:#990099;">BACKWARD</button>
+  </td>
+  <td>
+  <button data-cmd="128" style="background-color:#4d4dff;">BACKWARD RIGHT</button>
+  </td>
+</tr>
+</table>
+</br>
+<table>
+<tr>
+  <td>
+  <button data-cmd="256" style="background-color:#e68a00;">SPIN LEFT</button>
+  </td>
+  <td>
+  <button data-cmd="512" style="background-color:#aaaa55;">SPIN RIGHT</button>
+  </td>
+</tr>
+</table>
+</center>
 </div>
-<p>
-Remote Control : <span id="ws_state" style="color:blue">Not Connected</span><br>
+<p font-color: #404040;>
+<center>
+Remote Control 
+</br>
+<span id="ws_state" style="color:blue">Not Connected</span>
+</br>
+</center>
 </p>
 <button id="wc_conn" type="button" onclick="wc_onclick();">Connect to Soccer Robot</button>
 <br><br>
